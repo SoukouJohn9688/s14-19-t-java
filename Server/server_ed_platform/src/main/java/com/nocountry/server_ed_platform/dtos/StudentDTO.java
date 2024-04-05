0aecfa9a4ffb8153ad1d2e31573288fd2dd2679a
Leonel Amaya <omar.mejia2099@gmail.com>
2024-04-05 12:22:16 -0500

    Add Student service and its DTOs

---
+++ Server/server_ed_platform/src/main/java/com/nocountry/server_ed_platform/dtos/StudentDTO.java
@@ -0,0 +1,12 @@
+package com.nocountry.server_ed_platform.dtos;
+
+import java.time.LocalDate;
+
+public class StudentDTO {
+    private Long student_id;
+    private String name;
+    private String surname;
+    private LocalDate birthdate;
+    private String grade;
+    private String email;
+}
