58c5a6e1b8eba36c4d3cc921944c1a0dd8bf75d5
Teche95 <silencertec@gmail.com>
2024-04-10 16:03:09 -0300

    se agrega calendario de asistencias con opcion de borrado

--- Client/react-nocountri-project/src/App.jsx
+++ Client/react-nocountri-project/src/App.jsx
@@ -1,6 +1,6 @@
 /* import Login from "./components/Login"; */
 import Navbar from "./components/Navbar/Navbar";
-import Asistencias from "./pages/Asistencias/Asistencias";
+import Asistencias from "./pages/CalendarioAsistencias/CalendarioAsistencias";
 import Router from "./router/Router";
 import Footer from "./components/ui/Footer";
 
