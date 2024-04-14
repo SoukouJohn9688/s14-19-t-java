5aa98dde51cc2c00747b99271cc89e56c6c65541
Leonel Amaya <omar.mejia2099@gmail.com>
2024-04-13 19:16:08 -0500

    Added Student not found exception

---
+++ Server/server_ed_platform/src/main/java/com/nocountry/server_ed_platform/exceptions/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nocountry.server_ed_platform.exceptions;
+
+public class StudentNotFoundException extends Exception{
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
